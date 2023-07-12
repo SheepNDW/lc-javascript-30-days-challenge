c0f2c44956fe88709a8b661737ecd706aacc8b12
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2023-07-12 08:49:20 +0800

    test: update test

--- problems/Day13/promisePool.spec.ts
+++ problems/Day13/promisePool.spec.ts
@@ -29,19 +29,19 @@ describe('promisePool', () => {
 
   it('should execute n functions at the same time', async () => {
     const calls: number[] = [];
-    const start = Date.now();
     const functions = [
       mockAsyncFunction(10, calls),
       mockAsyncFunction(10, calls),
       mockAsyncFunction(10, calls),
     ];
+    const start = Date.now();
     await promisePool(functions, 2);
     const end = Date.now();
     const duration = end - start;
 
     // 檢查是否同時執行了 n 個函式
-    expect(duration).toBeGreaterThanOrEqual(20);
-    expect(duration).toBeLessThan(35);
+    expect(duration).toBeGreaterThanOrEqual(10);
+    expect(duration).toBeLessThan(30);
 
     // 檢查是否所有的函式都被執行了
     expect(calls.length).toBe(3);
@@ -54,15 +54,13 @@ describe('promisePool', () => {
       mockAsyncFunction(40, calls),
       mockAsyncFunction(20, calls),
     ];
-    const start = Date.now();
     await promisePool(functions, 1);
-    const end = Date.now();
-    const duration = end - start;
-
-    // 檢查是否按照序列順序執行了函式
-    expect(duration).toBeGreaterThanOrEqual(30 + 40 + 20);
 
     // 檢查是否所有的函式都被執行了
     expect(calls.length).toBe(3);
+
+    // 檢查調用順序
+    expect(calls[0]).toBeLessThan(calls[1]);
+    expect(calls[1]).toBeLessThan(calls[2]);
   });
 });
